eed8ebd02273acf544b9257bd7b766c07432b635
Tomasz <t.moniuszkogd@gmail.com>
2022-01-25 21:51:13 +0100

    Zadanie 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -2,9 +2,68 @@ package com.company;
 
 public class Animal
 {
-    String species;
+    private  static final Double DEFAULT_ANIMAL_WEIGHT = 1.3;
+    private  static final Double DEFAULT_DOG_WEIGHT = 13.0;
+    private  static final Double DEFAULT_CAT_WEIGHT = 2.3;
+
+    final String species;
     String name;
-    Double weight;
+    private Double weight;
     Integer age;
     Boolean alive;
+
+    public Animal (String species)
+    {
+        this.species = species;
+        this.alive = true;
+        if ( this.species.equals("pies"))
+        {
+            this.weight = DEFAULT_DOG_WEIGHT;
+        }
+        else if (this.species.equals("kot"))
+        {
+            this.weight = DEFAULT_CAT_WEIGHT;
+        }
+        else
+        {
+            this.weight = DEFAULT_ANIMAL_WEIGHT;
+        }
+    }
+    void feed (Double foodWeight)
+    {
+        if (weight <= 0)
+        {
+            isAlive();
+        }
+        else
+        {
+            weight += foodWeight;
+            System.out.println("Ważę "+getWeight()+" kg.");
+        }
+    }
+    Double getWeight()
+    {
+        return weight;
+    }
+    void isAlive()
+    {
+        alive = false;
+        System.out.println("Pancio nie ma opcji wylądowałem w raju dla modelek. Już ani nie pojem ani nie pobiegam ;(");
+    }
+    void takeForWalk()
+    {
+        if (weight<= 0)
+        {
+            isAlive();
+        }
+        else
+        {
+            weight -= 1.5;
+            System.out.println("Spaliłem całe jedzonko! Teraz ważę "+weight+"kg");
+        }
+    }
+    void animalStat()
+    {
+        System.out.println("Jestem "+species+" o imieniu "+name+". Żyję od  "+age+" lat, a ważę "+getWeight()+" kg");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,44 @@ public class Main {
 
     public static void main(String[] args)
     {
-	// write your code here
+	  Animal dog = new Animal("pies");
+      dog.age = 4;
+      dog.name = "Aggro";
+      Animal cat = new Animal("kot");
+      cat.age = 2;
+      cat.name = "Niau Niau";
+
+      Double dogWeightBeforEating = dog.getWeight();
+
+      dog.animalStat();
+
+
+      dog.feed(1.0);
+      dog.feed(1.0);
+      System.out.println("Po jedzeniu przytyłem "+(dog.getWeight() - dogWeightBeforEating+ "kg"));
+
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+      dog.takeForWalk();
+
+      dog.feed(2.0);
+      cat.animalStat();
+      cat.feed(1.0);
+      cat.feed(1.0);
+      cat.takeForWalk();
+      cat.takeForWalk();
+      cat.takeForWalk();
+      cat.takeForWalk();
+      cat.feed(1.0);
+
+
+
     }
 }
